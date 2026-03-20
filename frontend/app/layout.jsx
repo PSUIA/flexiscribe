30b0882d0b0fc09ce5c9ba0037aa85bef5aedc94
Jevi <jyvrelperalta0134@gmail.com>
2026-03-20 10:35:14 +0800

    chore(init): add next.js app directory structure

---
+++ frontend/app/layout.jsx
@@ -0,0 +1,29 @@
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+import Footer from "@/components/shared/Footer";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+export const metadata = {
+  title: "fLexiScribe",
+  icons: {
+    icon: "/fLexiScribe-logo.ico",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} flex flex-col min-h-screen`}
+        suppressHydrationWarning
+      >
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
